9bf53c53adb82e8d2c76161f0ff6b411644fd7d5
pivovarit <gpiwowarek@gmail.com>
2019-10-03 01:40:06 +0200

    Init

---
+++ 01-intro/src/main/java/com/for_comprehension/function/demo/Demo.java
@@ -0,0 +1,6 @@
+package com.for_comprehension.function.demo;
+
+class Demo {
+    public static void main(String[] args) {
+    }
+}
---
+++ 01-intro/src/test/java/com/for_comprehension/function/DemoTest.java
@@ -0,0 +1,18 @@
+package com.for_comprehension.function;
+
+import org.junit.Test;
+
+import java.util.concurrent.Callable;
+import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+
+public class DemoTest {
+
+    @Test
+    public void example_0() throws Exception {
+    }
+}
