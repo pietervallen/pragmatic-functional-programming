70ee697a5bb3b10e602ef395cf02cfcc263f1e5d
pivovarit <gpiwowarek@gmail.com>
2019-10-07 10:31:40 +0200

    Functional Interfaces

--- 01-intro/src/main/java/com/for_comprehension/function/demo/Demo.java
+++ 01-intro/src/main/java/com/for_comprehension/function/demo/Demo.java
@@ -2,5 +2,17 @@ package com.for_comprehension.function.demo;
 
 class Demo {
     public static void main(String[] args) {
+        Runnable hello_inner_class = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hello from another thread");
+            }
+        };
+
+        //Runnable hello_lambda = () -> System.out.println("Hello from another thread");
+
+        Thread thread = new Thread(hello_inner_class);
+
+        thread.start();
     }
 }
--- 01-intro/src/test/java/com/for_comprehension/function/DemoTest.java
+++ 01-intro/src/test/java/com/for_comprehension/function/DemoTest.java
@@ -2,7 +2,6 @@ package com.for_comprehension.function;
 
 import org.junit.Test;
 
-import java.util.concurrent.Callable;
 import java.util.function.BinaryOperator;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -10,9 +9,48 @@ import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 
+// Optional/Stream/CompletableFuture
+// Lambda Expressions
+// Function Composition
+// Functional Interfaces/Lambda Expressions Under The Hood
 public class DemoTest {
 
     @Test
     public void example_0() throws Exception {
+        Runnable hello_inner_class = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hello from another thread");
+            }
+        };
+
+        Runnable hello_lambda = () -> System.out.println("Hello from another thread");
+
+        Thread thread = new Thread(hello_inner_class);
+
+        thread.start();
+    }
+
+    @Test
+    public void example_2() throws Exception {
+        Function<Integer, Integer> fun = integer -> 42 + 24;
+
+        // Function<Integer, Void>
+        Consumer<Integer> consumer = integer -> System.out.println(integer);
+
+        // Function<Void, Integer>
+        Supplier<Integer> supplier = () -> 42;
+
+        // BiFunction<Integer, Integer, Integer>
+        BinaryOperator<Integer> sum = (i1, i2) -> i1 + i2;
+
+        // Function<Integer, Integer>
+        UnaryOperator<Integer> unaryOperator = i -> i + 1;
+
+        // Function<Void, Void>
+        Runnable runnable = () -> System.out.println();
+
+        // Function<Integer, Boolean>
+        Predicate<Integer> predicate = i -> i > 42;
     }
 }
