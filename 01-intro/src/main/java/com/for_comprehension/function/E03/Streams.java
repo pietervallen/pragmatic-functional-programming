d97588782e9badd4a13af13e5df01436e0ac245d
pivovarit <gpiwowarek@gmail.com>
2019-10-08 09:58:19 +0200

    Add streams demo

--- 01-intro/src/main/java/com/for_comprehension/function/E03/Streams.java
+++ 01-intro/src/main/java/com/for_comprehension/function/E03/Streams.java
@@ -9,6 +9,7 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
@@ -144,7 +145,17 @@ class Streams {
      */
     static Function<List<Optional<Integer>>, List<Integer>> L12_filterPresent() {
         return list -> {
-            return null;
+            return list.stream()
+              .flatMap(optional -> toStream(optional))
+              //.filter(Optional::isPresent)
+              //.map(Optional::get)
+              .collect(Collectors.toList());
         };
+    }
+
+    static <T> Stream<T> toStream(Optional<T> optional) {
+        return optional
+          .map(Stream::of)
+          .orElseGet(Stream::empty);
     }
 }
