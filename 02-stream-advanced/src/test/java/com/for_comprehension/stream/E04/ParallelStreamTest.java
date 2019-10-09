86f843835bb79fc9736b26b0a1edaebdfc43a4df
pivovarit <gpiwowarek@gmail.com>
2019-10-09 12:56:34 +0200

    Improve the test

--- 02-stream-advanced/src/test/java/com/for_comprehension/stream/E04/ParallelStreamTest.java
+++ 02-stream-advanced/src/test/java/com/for_comprehension/stream/E04/ParallelStreamTest.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,12 +16,13 @@ public class ParallelStreamTest {
 
     @Test
     public void parallelSync() {
-        ExecutorService executor = Executors.newFixedThreadPool(4);
+        AtomicInteger counter = new AtomicInteger();
         List<Integer> list = Arrays.asList(1, 2, 3, 4);
 
-        long seconds = countSeconds(() -> ParallelStream.parallelSync(list, i -> withDelay(i), executor));
+        long seconds = countSeconds(() -> ParallelStream.parallelSync(list, i -> withDelay(i), new CountingExecutorService(Executors.newFixedThreadPool(4), counter)));
 
         assertThat(seconds).isEqualTo(1);
+        assertThat(counter.get()).isEqualTo(4);
     }
 
     @Test
