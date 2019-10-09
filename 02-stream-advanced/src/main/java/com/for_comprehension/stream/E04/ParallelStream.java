5b5f05702c03d2bb8e299ddb2fd0a9415aba9bc3
Vallen, P.H.C. (Pieter) <Pieter.Vallen@ing.com>
2019-10-09 14:58:36 +0200

    Advanced exercise

--- 02-stream-advanced/src/main/java/com/for_comprehension/stream/E04/ParallelStream.java
+++ 02-stream-advanced/src/main/java/com/for_comprehension/stream/E04/ParallelStream.java
@@ -1,10 +1,14 @@
 package com.for_comprehension.stream.E04;
 
+import com.sun.xml.internal.bind.v2.util.CollisionCheckStack;
+
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import static java.util.concurrent.CompletableFuture.allOf;
 import static java.util.concurrent.CompletableFuture.supplyAsync;
@@ -14,7 +18,10 @@ import static java.util.stream.Collectors.toList;
 class ParallelStream {
 
     static <T, R> List<R> parallelSync(Collection<T> input, Function<T, R> task, ExecutorService executor) {
-        return null;
+        return input.stream()
+                .map(elem -> CompletableFuture.supplyAsync(() -> task.apply(elem), executor))
+                .map(CompletableFuture::join)
+                .collect(Collectors.toList());
     }
 
     static <T, R> CompletableFuture<List<R>> parallelAsync(Collection<T> input, Function<T, R> task, ExecutorService executor) {
